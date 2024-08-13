79eefba439b41e00884840ddbb22e6b585f3a5b4
TriggerJames <Kimothojames254@gmail.com>
2024-08-13 21:24:57 +0300

    0x03.
    ES6 data manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,23 @@
 // Function to get a list of students
 export default function getListStudents() {
-  // Returning an array of student objects
-  return [
-    {
-      id: 1, // Unique identifier for the student
-      firstName: 'Guillaume', // First name of the student
-      location: 'San Francisco', // Location of the student
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    },
-  ];
-}
+    // Returning an array of student objects
+    return [
+      { 
+        id: 1,           // Unique identifier for the student
+        firstName: 'Guillaume', // First name of the student
+        location: 'San Francisco' // Location of the student
+      },
+      { 
+        id: 2, 
+        firstName: 'James', 
+        location: 'Columbia' 
+      },
+      { 
+        id: 5, 
+        firstName: 'Serena', 
+        location: 'San Francisco' 
+      },
+    ];
+  }
+
+  
